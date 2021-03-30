63311c9bb26a41be5301bfa40f61c22509cbb317
jffernandez <jffernandez@fenix-es.com>
2021-03-30 21:01:29 +0200

    Feat: Initial project setup

---
+++ test/shared/hardhatNode.ts
@@ -0,0 +1,15 @@
+import { network } from 'hardhat'
+
+export async function passOneHour(): Promise<void> {
+  await network.provider.request({
+    method: 'evm_increaseTime',
+    params: [3600],
+  })
+}
+
+export async function mineBlock(timestamp: number): Promise<void> {
+  await network.provider.request({
+    method: 'evm_mine',
+    params: [timestamp],
+  })
+}
