00b4922822930e3169c9a9d1e52806c6b7fa0abb
Jose F. Fernandez <jffernandez@fenix-es.com>
2021-04-19 17:57:32 +0200

    test: Add vault management tests

--- test/vaults/BalleMaster.spec.ts
+++ test/vaults/BalleMaster.spec.ts
@@ -1,4 +1,5 @@
 import { Contract, ContractFactory } from '@ethersproject/contracts'
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/dist/src/signer-with-address'
 import { ethers, deployments } from 'hardhat'
 import { BigNumber } from 'ethers'
 import { expect } from '../shared/expect'
@@ -8,12 +9,16 @@ describe('BalleMaster', () => {
   let balle: Contract
   let BalleMaster: ContractFactory
   let balleMaster: Contract
+  let testStrategy: Contract
+  let testLP: Contract
+  let deployer: SignerWithAddress, test: SignerWithAddress
 
   before('Load contract factory and deploy contracts', async () => {
     await deployments.fixture()
     balle = await ethers.getContract('BALLEv2')
 
     BalleMaster = await ethers.getContractFactory('BalleMaster')
+    ;({ deployer, test } = await ethers.getNamedSigners())
   })
 
   describe('Test constructor', () => {
@@ -35,35 +40,112 @@ describe('BalleMaster', () => {
     before('Deploy contracts', async () => {
       await deployments.fixture()
       balleMaster = await ethers.getContract('BalleMaster')
+      testStrategy = await ethers.getContract('TestStrategy')
+      testLP = await ethers.getContract('TestLP')
     })
 
-    it('should revert if anyone (not owner) try to add new vault')
-    it('should add new vault #0')
-    it('should get total vault count')
-    it('should revert if anyone (not owner) try to activate vault #0 rewards')
-    it('should revert if try to activate rewards on non existent vault')
-    it('should revert if try to activate vault #0 rewards with 0 allocPoint')
-    it('should activate vault #0 rewards')
-    it('should revert if try to activate an already activated vault')
-    it('should add new vault #1')
-    it('should activate vault #1 rewards')
-    it('should update totalAllocPoint')
-    it('should revert if anyone (not owner) try to modify vault #1 rewards')
-    it('should revert if try to modify rewards on non existent vault')
-    it('should revert if try to modify vault #1 rewards with 0 allocPoint')
-    it('should modify vault #1 rewards')
-    it('should update totalAllocPoint')
-    it('should revert if anyone (not owner) try to deactivate vault #1 rewards')
-    it('should revert if try to deactivate rewards on non existent vault')
-    it('should deactivate vault #1 rewards')
-    it('should revert if try to deactivate already deactivated vault #1')
-    it('should update totalAllocPoint')
+    it('should revert if anyone (not owner) try to add new vault', async () => {
+      expect(
+        balleMaster.connect(test).addVault(testLP.address, testLP.address, testStrategy.address),
+      ).to.be.revertedWith('Ownable: caller is not the owner')
+    })
+
+    it('should add new vault #0', async () => {
+      balleMaster.connect(deployer).addVault(testLP.address, testLP.address, testStrategy.address)
+    })
+
+    it('should get total vault count', async () => {
+      expect(await balleMaster.vaultLength()).to.be.equal(1)
+    })
+
+    it('should revert if anyone (not owner) try to activate vault #0 rewards', async () => {
+      expect(balleMaster.connect(test).activateVaultRewards(0, 100)).to.be.revertedWith(
+        'Ownable: caller is not the owner',
+      )
+    })
+
+    it('should revert if try to activate rewards on non existent vault', async () => {
+      expect(balleMaster.activateVaultRewards(99, 100)).to.be.revertedWith('!vault')
+    })
+
+    it('should revert if try to activate vault #0 rewards with 0 allocPoint', async () => {
+      expect(balleMaster.activateVaultRewards(0, 0)).to.be.revertedWith('!allocpoint')
+    })
+
+    it('should activate vault #0 rewards', async () => {
+      expect(balleMaster.activateVaultRewards(0, 100)).to.emit(balleMaster, 'ActivateRewards').withArgs(0, 100)
+    })
+
+    it('should revert if try to activate an already activated vault', async () => {
+      expect(balleMaster.activateVaultRewards(0, 100)).to.be.revertedWith('active')
+    })
+
+    it('should add new vault #1', async () => {
+      balleMaster.addVault(testLP.address, testLP.address, testStrategy.address)
+    })
+
+    it('should revert if try to modify rewards on non active vault', async () => {
+      expect(balleMaster.modifyVaultRewards(1, 200)).to.be.revertedWith('!active')
+    })
+
+    it('should activate vault #1 rewards', async () => {
+      expect(balleMaster.activateVaultRewards(1, 200)).to.emit(balleMaster, 'ActivateRewards').withArgs(1, 200)
+    })
+
+    it('should update totalAllocPoint', async () => {
+      expect(await balleMaster.totalAllocPoint()).to.be.equal(300)
+    })
+
+    it('should revert if anyone (not owner) try to modify vault #1 rewards', async () => {
+      expect(balleMaster.connect(test).modifyVaultRewards(1, 100)).to.be.revertedWith(
+        'Ownable: caller is not the owner',
+      )
+    })
+
+    it('should revert if try to modify rewards on non existent vault', async () => {
+      expect(balleMaster.modifyVaultRewards(99, 100)).to.be.revertedWith('!vault')
+    })
+
+    it('should revert if try to modify vault #1 rewards with 0 allocPoint', async () => {
+      expect(balleMaster.modifyVaultRewards(1, 0)).to.be.revertedWith('!allocpoint')
+    })
+
+    it('should modify vault #1 rewards', async () => {
+      expect(balleMaster.modifyVaultRewards(1, 500)).to.emit(balleMaster, 'ModifyRewards').withArgs(1, 500)
+    })
+
+    it('should update totalAllocPoint', async () => {
+      expect(await balleMaster.totalAllocPoint()).to.be.equal(600)
+    })
+
+    it('should revert if anyone (not owner) try to deactivate vault #1 rewards', async () => {
+      expect(balleMaster.connect(test).deactivateVaultRewards(1)).to.be.revertedWith('Ownable: caller is not the owner')
+    })
+
+    it('should revert if try to deactivate rewards on non existent vault', async () => {
+      expect(balleMaster.deactivateVaultRewards(99)).to.be.revertedWith('!vault')
+    })
+
+    it('should deactivate vault #1 rewards', async () => {
+      expect(balleMaster.deactivateVaultRewards(1)).to.emit(balleMaster, 'DeactivateRewards').withArgs(1)
+    })
+
+    it('should revert if try to deactivate already deactivated vault #1', async () => {
+      expect(balleMaster.deactivateVaultRewards(1)).to.be.revertedWith('!active')
+    })
+
+    it('should update totalAllocPoint', async () => {
+      expect(await balleMaster.totalAllocPoint()).to.be.equal(100)
+    })
   })
 
   describe('Deposit, Withdraw & Harvest', () => {
     before('Deploy contracts', async () => {
       await deployments.fixture()
+      balle = await ethers.getContract('BALLEv2')
       balleMaster = await ethers.getContract('BalleMaster')
+      testStrategy = await ethers.getContract('TestStrategy')
+      testLP = await ethers.getContract('TestLP')
     })
 
     it('should revert if deposit to non existent vault')
@@ -85,7 +167,10 @@ describe('BalleMaster', () => {
   describe('Deposit & Withdraw when wantToken != depositToken', () => {
     before('Deploy contracts', async () => {
       await deployments.fixture()
+      balle = await ethers.getContract('BALLEv2')
       balleMaster = await ethers.getContract('BalleMaster')
+      testStrategy = await ethers.getContract('TestStrategy')
+      testLP = await ethers.getContract('TestLP')
     })
 
     it('should deposit from user 1')
@@ -99,7 +184,10 @@ describe('BalleMaster', () => {
   describe('Rewards calculation', () => {
     before('Deploy contracts', async () => {
       await deployments.fixture()
+      balle = await ethers.getContract('BALLEv2')
       balleMaster = await ethers.getContract('BalleMaster')
+      testStrategy = await ethers.getContract('TestStrategy')
+      testLP = await ethers.getContract('TestLP')
     })
 
     it('should have testing reward parameters', async () => {
@@ -146,7 +234,10 @@ describe('BalleMaster', () => {
   describe('Emergency functions', () => {
     before('Deploy contracts', async () => {
       await deployments.fixture()
+      balle = await ethers.getContract('BALLEv2')
       balleMaster = await ethers.getContract('BalleMaster')
+      testStrategy = await ethers.getContract('TestStrategy')
+      testLP = await ethers.getContract('TestLP')
     })
 
     it('should deposit from user 1')
