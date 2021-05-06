1a89f0f7b901e675d73d770e4af9ec2d2412ba9b
Jose F. Fernandez <jffernandez@fenix-es.com>
2021-05-07 00:53:39 +0200

    feat: Add testnet support contracts

--- src/deploy/deployTestMasterChef.ts
+++ src/deploy/deployTestMasterChef.ts
@@ -5,10 +5,11 @@ const deploy: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployments, getNamedAccounts } = hre
   const { deploy } = deployments
   const { deployer } = await getNamedAccounts()
+  const CAKE = await deployments.get('CAKE')
 
-  await deploy('CAKE', {
+  await deploy('MasterChef', {
     from: deployer,
-    args: [],
+    args: [CAKE.address],
     log: true,
     deterministicDeployment: false,
   })
@@ -17,15 +18,12 @@ const deploy: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 deploy.skip = async (hre: HardhatRuntimeEnvironment) => {
   const { network } = hre
 
-  if (network.name == 'hardhat') {
-    // deploy only for tests
-    return false
-  }
   if (network.name == 'bsc_testnet') {
     // deploy to testnet
     return false
   }
   return true
 }
-deploy.tags = ['CAKE']
+deploy.tags = ['MasterChef']
+deploy.dependencies = ['CAKE']
 export default deploy
