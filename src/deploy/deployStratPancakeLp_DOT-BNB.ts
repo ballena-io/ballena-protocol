a2cbff906c19ca3b4a6c3d809fda414752888fee
Jose F. Fernandez <jffernandez@fenix-es.com>
2021-05-25 17:55:22 +0200

    feat: Add PancakeLP DOT-BNB Strat deploy script

---
+++ src/deploy/deployStratPancakeLp_DOT-BNB.ts
@@ -0,0 +1,52 @@
+import { HardhatRuntimeEnvironment } from 'hardhat/types'
+import { DeployFunction } from 'hardhat-deploy/types'
+
+const deploy: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployments, getNamedAccounts } = hre
+  const { deploy } = deployments
+  const { deployer, harvester, wbnb } = await getNamedAccounts()
+  const BalleRewardFund = await deployments.get('BalleRewardFund')
+  const BalleTreasury = await deployments.get('BalleTreasury')
+  const BALLE = await deployments.get('BALLEv2')
+  const BalleMaster = await deployments.get('BalleMaster')
+  const cake = '0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82' // only mainnet address needed
+  const dot = '0x7083609fce4d1d8dc0c979aab8c869ea2c873402' // only mainnet address needed
+  const pid = 255 // MasterChef pool ID
+  const addresses = [
+    '0xDd5bAd8f8b360d76d12FdA230F8BAF42fe0022CF', // LP Token address
+    wbnb,
+    dot,
+    cake,
+    '0x10ED43C718714eb63d5aA57B78B54704E256024E', // Router address
+    '0x73feaa1eE314F8c655E354234017bE2193C9E24E', // PCS MasterChef address
+    BalleMaster.address,
+    harvester,
+    BalleRewardFund.address,
+    BalleTreasury.address,
+  ]
+  const cakeToBallePath = [cake, wbnb, BALLE.address]
+  const cakeToToken0Path = [cake, wbnb]
+  const cakeToToken1Path = [cake, wbnb, dot]
+
+  await deploy('StratPancakeLp_DOT-BNB', {
+    contract: 'StratPancakeLpV1',
+    from: deployer,
+    args: [addresses, pid, cakeToBallePath, cakeToToken0Path, cakeToToken1Path],
+    log: true,
+    deterministicDeployment: false,
+    gasLimit: 3750000,
+  })
+}
+
+deploy.skip = async (hre: HardhatRuntimeEnvironment) => {
+  const { network } = hre
+
+  if (network.name == 'bsc_mainnet') {
+    // deploy only on bsc_mainnet
+    return false
+  }
+  return true
+}
+deploy.tags = ['StratPancakeLp_DOT-BNB']
+deploy.dependencies = ['BalleRewardFund', 'BalleTreasury', 'BALLEv2', 'BalleMaster']
+export default deploy
