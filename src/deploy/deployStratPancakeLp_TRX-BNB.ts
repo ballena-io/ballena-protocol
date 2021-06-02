745db0677f098a02436c1fcf85af5a5fcf3b4ce8
Jose F. Fernandez <jffernandez@fenix-es.com>
2021-06-02 18:47:54 +0200

    feat: Add TRX-BNB vault

---
+++ src/deploy/deployStratPancakeLp_TRX-BNB.ts
@@ -0,0 +1,52 @@
+import { HardhatRuntimeEnvironment } from 'hardhat/types'
+import { DeployFunction } from 'hardhat-deploy/types'
+
+const deploy: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployments, getNamedAccounts } = hre
+  const { deploy } = deployments
+  const { deployer, harvester, wbnb } = await getNamedAccounts()
+  const BalleRewardFund = await deployments.get('BalleRewardFund')
+  const BalleTreasury = await deployments.get('BalleTreasury')
+  const BALLE = await deployments.get('BALLEv2')
+  const BalleMaster = await deployments.get('BalleMaster')
+  const cake = '0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82' // only mainnet address needed
+  const trx = '0x85eac5ac2f758618dfa09bdbe0cf174e7d574d5b' // only mainnet address needed
+  const pid = 368 // MasterChef pool ID
+  const addresses = [
+    '0x3cd338c3bb249b6b3c55799f85a589febbbff9dd', // LP Token address
+    wbnb,
+    trx,
+    cake,
+    '0x10ED43C718714eb63d5aA57B78B54704E256024E', // Router address
+    '0x73feaa1eE314F8c655E354234017bE2193C9E24E', // PCS MasterChef address
+    BalleMaster.address,
+    harvester,
+    BalleRewardFund.address,
+    BalleTreasury.address,
+  ]
+  const cakeToBallePath = [cake, wbnb, BALLE.address]
+  const cakeToToken0Path = [cake, wbnb]
+  const cakeToToken1Path = [cake, wbnb, trx]
+
+  await deploy('StratPancakeLp_TRX-BNB', {
+    contract: 'StratPancakeLpV3',
+    from: deployer,
+    args: [addresses, pid, cakeToBallePath, cakeToToken0Path, cakeToToken1Path],
+    log: true,
+    deterministicDeployment: false,
+    gasLimit: 3750000,
+  })
+}
+
+deploy.skip = async (hre: HardhatRuntimeEnvironment) => {
+  const { network } = hre
+
+  if (network.name == 'bsc_mainnet') {
+    // deploy only on bsc_mainnet
+    return false
+  }
+  return true
+}
+deploy.tags = ['StratPancakeLp_TRX-BNB']
+deploy.dependencies = ['BalleRewardFund', 'BalleTreasury', 'BALLEv2', 'BalleMaster']
+export default deploy
