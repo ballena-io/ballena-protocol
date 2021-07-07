56e3f3900eedf3ac73d17a4acdf1b12595fa0595
Jose F. Fernandez <jffernandez@fenix-es.com>
2021-07-07 22:21:09 +0200

    feat: Deploy CAKE and BUSD-BNB strategies

---
+++ src/deploy/deployStratPancakeCAKE.ts
@@ -0,0 +1,48 @@
+import { HardhatRuntimeEnvironment } from 'hardhat/types'
+import { DeployFunction } from 'hardhat-deploy/types'
+
+// Pancake CAKE Strategy data
+const stratName = 'StratPancakeCAKE'
+
+const deploy: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployments, getNamedAccounts } = hre
+  const { deploy } = deployments
+  const { deployer, harvester, wbnb } = await getNamedAccounts()
+  const BalleRewardFund = await deployments.get('BalleRewardFund')
+  const BalleTreasury = await deployments.get('BalleTreasury')
+  const BALLE = await deployments.get('BALLEv2')
+  const BalleMaster = await deployments.get('BalleMaster')
+  const cake = '0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82' // only mainnet address needed
+  const addresses = [
+    cake,
+    '0x10ED43C718714eb63d5aA57B78B54704E256024E', // Router address
+    '0x73feaa1eE314F8c655E354234017bE2193C9E24E', // PCS MasterChef address
+    BalleMaster.address,
+    harvester,
+    BalleRewardFund.address,
+    BalleTreasury.address,
+  ]
+  const cakeToBallePath = [cake, wbnb, BALLE.address]
+
+  await deploy(stratName, {
+    contract: 'StratPancakeCakeV2',
+    from: deployer,
+    args: [addresses, cakeToBallePath],
+    log: true,
+    deterministicDeployment: false,
+    gasLimit: 3750000,
+  })
+}
+
+deploy.skip = async (hre: HardhatRuntimeEnvironment) => {
+  const { network } = hre
+
+  if (network.name == 'bsc_mainnet') {
+    // deploy only on bsc_mainnet
+    return false
+  }
+  return true
+}
+deploy.tags = [stratName]
+deploy.dependencies = ['BalleRewardFund', 'BalleTreasury', 'BALLEv2', 'BalleMaster']
+export default deploy
